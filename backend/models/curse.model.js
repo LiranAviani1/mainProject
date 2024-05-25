737a6db1769157a85195f520950c5fed844fd1de
LiranAviani <lirankir1@gmail.com>
2024-05-25 20:08:35 +0300

    edit curse card

--- backend/models/curse.model.js
+++ backend/models/curse.model.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema;
 const curseSchema = new Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
-  category: { type: String, required: true},
-  subCategory: { type: String, required: true},
+  category: { type: String, required: true },
+  subCategory: { type: String, required: true },
   dateStart: { type: Date, default: new Date().getTime() },
   dateEnd: { type: Date, default: new Date().getTime() },
   capacity: { type: Number, default: 0 },
