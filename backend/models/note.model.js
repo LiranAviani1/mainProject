6bd2d18e3b0841f508421eb16c9774bbe4dc56f2
LiranAviani <lirankir1@gmail.com>
2024-05-25 14:57:28 +0300

    first one

--- backend/models/note.model.js
+++ backend/models/note.model.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 const noteSchema = new Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
-  tags: { type: [String], default: [] },
-  isPinned: { type: Boolean, default: false },
-  userId: { type: String, required: true },
+  category: { type: [String], default: [] },
+  subCategory: { type: [String], default: [] },
+  
   createdOn: { type: Date, default: new Date().getTime() },
 });
 
