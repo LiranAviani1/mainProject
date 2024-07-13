d36cc6a509f0ffa78cf24a015e9d4d99721235f6
LiranAviani <lirankir1@gmail.com>
2024-07-13 15:00:42 +0300

    fix button

--- frontend/courses-app/src/pages/Home/Home.jsx
+++ frontend/courses-app/src/pages/Home/Home.jsx
@@ -178,7 +178,7 @@ const Home = () => {
       {userInfo &&
         (userInfo.role === "admin" || userInfo.role === "teacher") && (
           <button
-            className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10"
+            className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 fixed right-10 bottom-10"
             onClick={() => {
               setOpenAddEditModal({ isShown: true, type: "add", data: null });
             }}
