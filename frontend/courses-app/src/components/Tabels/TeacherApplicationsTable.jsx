6726615344a7d2ab6d72b185220248b66fffc8d2
LiranAviani <lirankir1@gmail.com>
2024-10-28 18:31:29 +0200

    add app date

--- frontend/courses-app/src/components/Tabels/TeacherApplicationsTable.jsx
+++ frontend/courses-app/src/components/Tabels/TeacherApplicationsTable.jsx
@@ -9,6 +9,7 @@ const TeacherApplicationsTable = ({ applications, onApprove, onDeny, onView }) =
             <th className="w-1/5 py-3 px-4 border-b text-sm font-semibold text-center">Full Name</th>
             <th className="w-1/5 py-3 px-4 border-b text-sm font-semibold text-center">Email</th>
             <th className="w-1/5 py-3 px-4 border-b text-sm font-semibold text-center">Phone</th>
+            <th className="w-1/5 py-3 px-4 border-b text-sm font-semibold text-center">Date</th>
             <th className="w-1/5 py-3 px-4 border-b text-sm font-semibold text-center">Status</th>
             <th className="w-1/5 py-3 px-4 border-b text-sm font-semibold text-center">Actions</th>
           </tr>
@@ -19,6 +20,7 @@ const TeacherApplicationsTable = ({ applications, onApprove, onDeny, onView }) =
               <td className="py-3 px-4 border-b text-center">{app.fullName}</td>
               <td className="py-3 px-4 border-b text-center">{app.email}</td>
               <td className="py-3 px-4 border-b text-center">{app.phone}</td>
+              <td className="py-3 px-4 border-b text-center">{new Date(app.appliedOn).toLocaleDateString('en-GB')}</td>
               <td className="py-3 px-4 border-b text-center">
                 {app.status === "approved" && (
                   <span className="text-green-600 font-semibold">Approved</span>
